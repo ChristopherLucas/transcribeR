8e1bbe15d8e51f33b5e1bd27c538fe24bdfccc0f
Shiv Sunil <shiv_sunil@comcast.net>
2015-07-24 13:11:30 -0400

    bug fixes

--- example.R
+++ example.R
@@ -6,7 +6,7 @@ printLanguages()
 
 sendAudioGetJobs(wav.dir = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/",
                  api.key = API_KEY, interval = "-1",
-                 encode = "multipart", existing.csv = NULL,
+                 encode = "multipart", existing.csv = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/test_jobs.csv",
                  csv.location = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/test_jobs.csv",
                  language = "en-US", verbose = FALSE)
 
