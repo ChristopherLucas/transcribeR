bee1a67c3eab39b48592e88cd8fe0dfc5439e897
Shiv Sunil <shiv_sunil@comcast.net>
2015-07-24 12:51:13 -0400

    bug fixes

--- example.R
+++ example.R
@@ -1,17 +1,17 @@
 library(devtools)
 install_github('ChristopherLucas/transcribeR/package')
+library(transcribeR)
 
-transcribeR::printLanguages()
+printLanguages()
 
-transcribeR::sendAudioGetJobs(wav.dir = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/",
-                 api.key = "d7d3d57d-22f9-4c04-8dc1-daaea492d5ce", interval = "-1",
+sendAudioGetJobs(wav.dir = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/",
+                 api.key = API_KEY, interval = "-1",
                  encode = "multipart", existing.csv = NULL,
                  csv.location = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/test_jobs.csv",
                  language = "en-US", verbose = FALSE)
 
-Sys.sleep(10)
 
-transcribeR::retrieveText.R(job.file = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/test_jobs.csv",
-               api.key = "d7d3d57d-22f9-4c04-8dc1-daaea492d5ce")
+retrieveText(job.file = "/Users/shivsunil/Desktop/Harvard_Research/transcribeR_testing/test_jobs.csv",
+               api.key = API_KEY)
 
 print("Done!")
