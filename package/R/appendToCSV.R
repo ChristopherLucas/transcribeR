adc1f8271f8271daf63d335ac9780e3b151ac2d0
Shiv Sunil <shiv_sunil@comcast.net>
2015-07-29 15:14:58 -0400

    edits for CRAN checks

--- package/R/appendToCSV.R
+++ package/R/appendToCSV.R
@@ -1,6 +1,6 @@
 appendToCSV <- function(existing.csv, row.frame, append = TRUE, sep=",", row.names=FALSE, col.names=FALSE) {
   if (is.null(existing.csv) || file.exists(existing.csv) == FALSE) {
-    warning("The file was not found or path was wrong, \"file.name=" + file.name + "\"")
+    warning("The file was not found or path was wrong, \"file.name=" + existing.csv+ "\"")
     return(FALSE)
   }
   row.names(row.frame) <- NULL
